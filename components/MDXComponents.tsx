09b9a1d4bcecd23c8af81ba57fe17a3df1dc1a9f
Esperanza Dueñas <da13002@ues.edu.sv>
2021-07-22 15:04:20 -0600

    feat: add MDX component

---
+++ components/MDXComponents.tsx
@@ -0,0 +1,25 @@
+import Link, { LinkProps } from "next/link";
+import React from "react";
+
+
+const CustomLink = (props: React.ComponentProps<"a"> & LinkProps) =>{
+  const href = props.href;
+  const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
+
+  if (isInternalLink) {
+    return (
+      <Link href={href}>
+        <a {...props}/>
+      </Link>
+    );
+  }
+
+  return <a target="_blank" rel="noopener noreferrer" {...props}/>
+}
+
+const MDXComponents = {
+  a: CustomLink,
+  CustomLink
+}
+
+export default MDXComponents;
