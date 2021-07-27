8b32fffc3082aa9a1c2b1d419ce46c7f37424047
Esperanza Dueñas <da13002@ues.edu.sv>
2021-07-27 14:49:37 -0600

    feat(components/Navbar): add hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,6 +23,31 @@ export const Navbar = () =>{
             Hopechan
           </span>
         </Link>
+        <button 
+          className='
+            inline-flex 
+            p-3 
+            hover:bg-green-600 
+            rounded lg:hidden 
+            text-white ml-auto 
+            hover:text-white 
+            outline-none'
+        >
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://wwww.w3.org/2000/svg'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M4 6h16M4 12h16M4 18h16'
+            />
+          </svg>
+        </button>
       </nav>
     </>
   );
